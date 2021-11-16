7aec8400df28e068cdd31c4094b54d4327806d06
Md Nazmul Hasan <farhapartex@gmail.com>
2021-11-16 19:45:14 +0600

    refactor css for responsiveness

--- src/pages/UserAuth.js
+++ src/pages/UserAuth.js
@@ -11,8 +11,8 @@ const UserAuth = () =>{
         <div className="w-100">
             <Container fluid>
                 <div className="row">
-                    <div className="col-8" id="loginLeftSide"></div>
-                    <div className="col-4">
+                    <div className="col-xl-8 col-lg-7 col-md-5 col-sm-1" id="loginLeftSide"></div>
+                    <div className="col-xl-4 col-lg-5 col-md-7 col-sm-11">
                         <div className="w-75 p-5 mx-auto" id="loginBaseBlock">
                             {isShowLoginBlock && <Login setIsShowLoginBlock={setIsShowLoginBlock}/>}
                             {!isShowLoginBlock && <Registration setIsShowLoginBlock={setIsShowLoginBlock}/>}
