93c378c6a89406d62c848848290863310c319a90
Md Nazmul Hasan <farhapartex@gmail.com>
2021-11-28 13:18:48 +0600

    fontawsome icon added

--- src/component/profile/profileBody.js
+++ src/component/profile/profileBody.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const ProfileBody = ()=> {
     return (
@@ -10,12 +12,13 @@ const ProfileBody = ()=> {
                         <div id="userIntro">
                             <h4>Intro</h4>
                             <ul className="activity-list mt-3">
-                                <li>Software Engineer at Sixads</li>
-                                <li>Former Software Engineer at Strativ</li>
-                                <li>Former Back-End Developer at COdesign</li>
-                                <li>Former Software Developer at Genweb2</li>
-                                <li>Former Student of Second Major (CSE) at Shahjalal University of Science & Technology, Sylhet</li>
-                                <li>Former Student at Department of Mathematics at Shahjalal University of Science & Technology, Sylhet</li>
+                                <li><span className="mr-1"><FontAwesomeIcon icon="briefcase" /></span><span>Software Engineer at Sixads</span></li>
+                                <li><span className="mr-1"><FontAwesomeIcon icon="briefcase" /></span><span>Former Software Engineer at Strativ</span></li>
+                                <li><span className="mr-1"><FontAwesomeIcon icon="briefcase" /></span><span>Former Back-End Developer at COdesign</span></li>
+                                <li><span className="mr-1"><FontAwesomeIcon icon="briefcase" /></span><span>Former Software Developer at Genweb2</span></li>
+                                <li><span className="mr-1"><FontAwesomeIcon icon="briefcase" /></span><span>Software Engineer at Sixads</span></li>
+                                <li><span className="mr-1"><FontAwesomeIcon icon="briefcase" /></span><span>Former Student of Second Major (CSE) at Shahjalal University of Science & Technology, Sylhet</span></li>
+                                <li><span className="mr-1"><FontAwesomeIcon icon="briefcase" /></span><span>Former Student at Department of Mathematics at Shahjalal University of Science & Technology, Sylhet</span></li>
                             </ul>
                         </div>
                     </div>
