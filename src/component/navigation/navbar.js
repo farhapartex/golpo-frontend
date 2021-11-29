3f583967ad1f9a00c3960d6226c5a6407355bc13
Md Nazmul Hasan <farhapartex@gmail.com>
2021-11-29 12:08:18 +0600

    home icon added

--- src/component/navigation/navbar.js
+++ src/component/navigation/navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 const NavigationBar = ()=> {
@@ -10,15 +11,8 @@ const NavigationBar = ()=> {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mx-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link href="/"><span className="icon-space"><FontAwesomeIcon icon="home" /></span>Home</Nav.Link>
+                <Nav.Link href="#link"><span className="icon-space"><FontAwesomeIcon icon="comment-alt" /></span>Messenger</Nav.Link>
             </Nav>
             <Nav>
                 <Nav.Link href="/profile">Profile</Nav.Link>
--- src/component/post/SinglePost.js
+++ src/component/post/SinglePost.js
@@ -11,7 +11,7 @@ const SinglePost = () => {
                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when 
                     looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal 
                     distribution of letters, as opposed to using 'Content here, content here', making it look like 
-                    readable English. Many desktop publishing packages and web page editors ... <a href="#">See more</a></p>
+                    readable English. Many desktop publishing packages and web page editors ... <a href="/">See more</a></p>
             </div>
         </div>
     )
